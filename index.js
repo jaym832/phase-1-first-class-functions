193bb60d16137c58a9a3bef6eec8af0a429aeff2
Javier Marin <jjmarin777@gmail.com>
2021-04-07 19:22:15 -0500

    finished

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+function receivesAFunction(callback){
+    callback();
+
+}
+
+function returnsANamedFunction(){
+    return function callback(){}
+}
+
+function returnsAnAnonymousFunction(){
+     return function(){
+
+    }
+}
